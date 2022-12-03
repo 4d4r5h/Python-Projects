8dedede0b6b4b2c284a24fde874c2b2e530c147e
Adarsh <46349391+4d4r5h@users.noreply.github.com>
2022-12-03 18:27:59 +0530

    Update Drawing_Circles.py

--- Drawing_Circles.py
+++ Drawing_Circles.py
@@ -1,27 +1,27 @@
 import pyautogui, math, time
 
-time.sleep(3)       # Open Paint in this time frame
+time.sleep(3)       # open paint in this time frame
 i = 828
 j = 60
-print(pyautogui.size())        # For mine, the output is Size(width=1366, height=768). Checkout yours.
+print(pyautogui.size())        # For mine, the output is Size(width=1366, height=768)
 
 for radius in range(20, 220, 20):
-    pyautogui.click(325, 70)        # Click on Brush
-    pyautogui.moveTo(768, 60)       # Pick Black color
+    pyautogui.click(325, 70)        # click on brush
+    pyautogui.moveTo(768, 60)       # pick black color
     pyautogui.click()
     t = 0
     while t <= 360:
-        x = 600 + round(radius * math.cos(t * (math.pi / 180)))         # Using Parametric equation of Circle
+        x = 600 + round(radius * math.cos(t * (math.pi / 180)))         # using parametric equation of circle
         y = 400 + round(radius * math.sin(t * (math.pi / 180)))
         if t == 0:
             pyautogui.moveTo(x, y)
         else:
             pyautogui.dragTo(x, y)
-        t += 10         # For smooth circle
+        t += 10         # for smooth circle
 
-    pyautogui.click(265, 70)        # Click on Bucket
+    pyautogui.click(265, 70)        # click on bucket
     if i < 970:
-        pyautogui.moveTo(i, j)      # Picking different colors
+        pyautogui.moveTo(i, j)      # picking different colors
         i += 21
     else:
         i = 828
